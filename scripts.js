b0d111d195d449b2d8210f2f6ff025db05fc9e21
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-05-24 17:43:06 -0300

    convert function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
+const USD = 5.17;
+const EUR = 5.61;
+const GBP = 6.58;
+
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 amount.addEventListener('input', ()=>{
   const hasCharacteresRegex = /\D+/g
   amount.value = amount.value.replace(hasCharacteresRegex, '');
-})
+});
+
+form.onsubmit = (event) =>{
+  event.preventDefault();
+
+  switch(currency.value){
+    case 'USD':
+      convertCurrency(amount.value, USD, 'US$')
+      break
+    case 'EUR':
+      convertCurrency(amount.value, EUR, '€')
+      break
+    case 'GBP':
+      convertCurrency(amount.value, GBP, '£')
+  }
+}
+
+function convertCurrency(amount, price, symbol){
+  console.log(amount);
+  console.log(price);
+  console.log(symbol);
+}
